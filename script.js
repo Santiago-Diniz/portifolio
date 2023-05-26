3037bed4a71ec0d83e0ddcc250b9de081674d24d
Santiago Diniz <santiagodiniz3@gmail.com>
2023-05-26 10:28:15 -0300

    add moving

--- script.js
+++ script.js
@@ -24,3 +24,18 @@ function menuMobol(){
 })
 }
 
+
+
+const myObserver = new IntersectionObserver ( (entries) => {
+  entries.forEach( (entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show')
+    }else{
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const elements = document.querySelectorAll('.moving')
+
+elements.forEach( (element) => myObserver.observe(element))
